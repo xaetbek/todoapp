7b8f0995b47c55f02eb644e7b1133afddf00c0d4
Khayotbek Azimov <xaetbek@gmail.com>
2020-12-20 16:18:35 -0800

    completed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,23 +6,36 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://Admin@localhost:5432/todoapp
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(), nullable=False)
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
 
-    def __repr__(self):
-        return f'<Todo {self.id} {seld.description}'
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort (400)
+    else:
+        return jsonify(body)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+  return render_template('index.html', data=Todo.query.all())
