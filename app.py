4687931df224a41c7bd2f566b6dcaa86af9aaf54
Khayotbek Azimov <xaetbek@gmail.com>
2020-12-27 23:23:54 -0800

    Bootstrapped database migrate commands;

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://Admin@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
+db.create_all()
+
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean(), nullable=False, default=False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
