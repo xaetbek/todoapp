0541b7bd63f62942f70709819cbbaeef70940a38
Khayotbek Azimov <xaetbek@gmail.com>
2021-01-02 09:57:29 -0800

    added update and delete functions

--- app.py
+++ app.py
@@ -55,6 +55,19 @@ def set_completed_todo(todo_id):
     return redirect(url_for('index'))
 
 
+@app.route('/todos/<todo_id>/deleted', methods=['DELETE'])
+def delete_todo(todo_id):
+  try:
+    Todo.query.filter_by(id=todo_id).delete()
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return jsonify({ 'success': True })
+
+
+
 @app.route('/')
 def index():
   return render_template('index.html', data=Todo.query.order_by('id').all())
