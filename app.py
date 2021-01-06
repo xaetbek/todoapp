7194a4d5ac70fea15ad02edc9b8eb91a1accb526
Khayotbek Azimov <xaetbek@gmail.com>
2021-01-06 08:43:02 -0800

    Added "one-to-many relationship" data model.

--- app.py
+++ app.py
@@ -11,13 +11,22 @@ migrate = Migrate(app, db)
 db.create_all()
 
 class Todo(db.Model):
-  __tablename__ = 'todos'
-  id = db.Column(db.Integer, primary_key=True)
-  description = db.Column(db.String(), nullable=False)
-  completed = db.Column(db.Boolean(), nullable=False, default=False)
 
-  def __repr__(self):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean(), nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
+
+
+def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
 
 
 @app.route('/todos/create', methods=['POST'])
