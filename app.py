4450a63a6d6f674c5fe08d2989075d4ec9fc06cd
Khayotbek Azimov <xaetbek@gmail.com>
2021-01-11 20:12:09 -0800

    Many-to-many db relationship is added.

--- app.py
+++ app.py
@@ -32,6 +32,21 @@ class TodoList(db.Model):
     def __repr__(self):
         return f'<TodoList ID: {self.id}, name: {self.name}, todos: {self.todos}>'
 
+order_items = db.Table('order_items',
+    db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+)
+
+class Order(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  status = db.Column(db.String(), nullable=False)
+  products = db.relationship('Product', secondary=order_items,
+      backref=db.backref('orders', lazy=True))
+
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     error = False
